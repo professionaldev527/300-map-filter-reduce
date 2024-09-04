f491c1955fe47adacb525b53b143918ca234e70e
professionaldev527 <professionaldev527@gmail.com>
2024-09-04 13:44:27 +0530

    Update

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+// var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+import emojipedia from "./emojipedia";
+
+// console.log(emojipedia);
+const meanings = emojipedia.map(emojiEntry => emojiEntry.meaning);
+
+const truncatedMeanings = emojipedia.map(function (emojiEntry) {
+     return emojiEntry.meaning.substring(0, 100)
+});
+  
+console.log(truncatedMeanings);
+
